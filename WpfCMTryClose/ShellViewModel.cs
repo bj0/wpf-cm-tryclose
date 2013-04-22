38a87e074e48ae91e2dc5e94e9fd2ee04878c994
Brian Parma <execrable@gmail.com>
2013-04-22 15:13:28 -0700

    add project

---
+++ WpfCMTryClose/ShellViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Caliburn.Micro;
+
+namespace WpfCMTryClose
+{
+    public class ShellViewModel : Screen
+    {
+        public MainViewModel MainView { get; protected set; }
+
+        public ShellViewModel()
+        {
+            MainView = new MainViewModel();
+        }
+
+        public void Exit()
+        {
+            TryClose();
+        }
+    }
+}
